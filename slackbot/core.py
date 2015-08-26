5ff4236c0305888b1dfeb41da75a4427388bc249
reorx <novoreorx@gmail.com>
2015-08-26 17:23:04 +0800

    add get_channels

--- slackbot/core.py
+++ slackbot/core.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import json
 from slackclient import SlackClient
 from . import settings
 
 
 client = SlackClient(settings.SLACK_TOKEN)
+
+
+def get_channels():
+    channels = {}
+    keep_keys = ['id', 'name', 'is_archived', 'is_member']
+    rv = json.loads(client.api_call('channels.list'))
+    for i in rv['channels']:
+        c = {k: v for k, v in i.iteritems() if k in keep_keys}
+        channels[c['id']] = c
+    return channels
