7fbb08b013e31d8c5ebe355d77df665e51b1214e
reorx <novoreorx@gmail.com>
2015-08-31 19:38:00 +0800

    rewrite mybot

--- mybot.py
+++ mybot.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from slackbot.wsbot import WSBot
+from slackbot import SlackBot
+from slackbot.web import SlackHandler
 import mybot_config
 
 
-bot = WSBot(mybot_config)
+bot = SlackBot(mybot_config)
 
 
-@bot.match_text(r'^/loki')
-def hello(message):
-    pass
+@bot.on_event('hello')
+def hello(msg):
+    return msg.reply("I'm online!", channel='slack-test')
+
+
+@bot.on_event('message')
+def repeat(msg):
+    return msg.reply('You just said: %s' % msg.raw['text'])
+
+
+@bot.route('/send')
+class SendHandler(SlackHandler):
+    def get(self):
+        text = self.get_argument('text')
+        channel = self.get_argument('channel')
+        channel_id = bot.channels.get(name=channel)['id']
+
+        bot.client.send_message(channel_id, text)
 
 
 if __name__ == '__main__':
